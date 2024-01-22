e8e131193044c044479387318c9d6cf185be406d
Moraes <moraes.tech01@gmail.com>
2024-01-22 20:51:40 -0300

    Efeito de Transição de navegação entre pages

--- driveease_v1/lib/Widgets/bottomNavigationBarCustom.dart
+++ driveease_v1/lib/Widgets/bottomNavigationBarCustom.dart
@@ -13,35 +13,67 @@ class BottomNavigationBarCustom extends StatefulWidget {
 
 int selectedIcon = 0;
 
-_vaiParaHome(BuildContext context) {
+_vaiParaHome(BuildContext context, int direction) {
   Navigator.of(context).pushReplacement(
-    MaterialPageRoute(
-      builder: (context) => const HomePage(),
+    _createRoute(
+      direction,
+      const HomePage(),
     ),
   );
 }
 
-_vaiParaRelatorio(BuildContext context) {
+_vaiParaRelatorio(BuildContext context, int direction) {
   Navigator.of(context).pushReplacement(
-    MaterialPageRoute(
-      builder: (context) => const ReportPage(),
+    _createRoute(
+      direction,
+      const ReportPage(),
     ),
   );
 }
 
-_vaiParaHistorico(BuildContext context) {
+_vaiParaHistorico(BuildContext context, int direction) {
   Navigator.of(context).pushReplacement(
-    MaterialPageRoute(
-      builder: (context) => const HistoricPage(),
+    _createRoute(
+      direction,
+      const HistoricPage(),
     ),
   );
 }
 
-_vaiParaMais(BuildContext context) {
+_vaiParaMais(BuildContext context, int direction) {
   Navigator.of(context).pushReplacement(
-    MaterialPageRoute(
-      builder: (context) => const HomePage(),
+    _createRoute(
+      direction,
+      const HomePage(),
     ),
+  );
+}
+
+Route _createRoute(int direction, StatelessWidget page) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      Offset begin;
+      Offset end;
+
+      if (direction > 0) {
+        begin = Offset(-1.0, 0.0);
+        end = Offset(0.0, 0.0);
+      } else if (direction == 0) {
+        begin = Offset(0.0, 0.0);
+        end = Offset(0.0, 0.0);
+      } else {
+        begin = Offset(1.0, 0.0);
+        end = Offset(0.0, 0.0);
+      }
+      const curve = Curves.easeOut;
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
   );
 }
 
@@ -58,29 +90,52 @@ class _BottomNavigationBarCustomState extends State<BottomNavigationBarCustom> {
         switch (value) {
           case 0:
             setState(() {
+              int direction = 0;
+              if (selectedIcon > value) {
+                direction = 1;
+              } else if (selectedIcon < value) {
+                direction = -1;
+              }
               selectedIcon = value;
-              _vaiParaHome(context);
+              _vaiParaHome(context, direction);
             });
-
             break;
           case 1:
             setState(() {
+              int direction = 0;
+              if (selectedIcon > value) {
+                direction = 1;
+              } else if (selectedIcon < value) {
+                direction = -1;
+              }
               selectedIcon = value;
-              _vaiParaRelatorio(context);
+              _vaiParaRelatorio(context, direction);
             });
 
             break;
           case 2:
             setState(() {
+              int direction = 0;
+              if (selectedIcon > value) {
+                direction = 1;
+              } else if (selectedIcon < value) {
+                direction = -1;
+              }
               selectedIcon = value;
-              _vaiParaHistorico(context);
+              _vaiParaHistorico(context, direction);
             });
 
             break;
           case 3:
             setState(() {
+              int direction = 0;
+              if (selectedIcon > value) {
+                direction = 1;
+              } else if (selectedIcon < value) {
+                direction = -1;
+              }
               selectedIcon = value;
-              _vaiParaMais(context);
+              _vaiParaMais(context, direction);
             });
 
             break;
