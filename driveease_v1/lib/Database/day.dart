cbb870922072e63cb19ce75a57ce61bf884ae859
Carlos Matheus <carlos.tecbnu@gmail.com>
2024-01-30 19:34:52 -0300

    Atualizações banco de dados

--- driveease_v1/lib/Database/day.dart
+++ driveease_v1/lib/Database/day.dart
@@ -1,14 +1,18 @@
 class Day {
   int? id;
-  String data;
+  int idCarro;
+  String dataHoraStart;
   double startKm;
+  String dataHoraStop;
   double stopKm;
   double ganhos;
 
   Day({
     this.id,
-    required this.data,
+    required this.idCarro,
+    required this.dataHoraStart,
     required this.startKm,
+    required this.dataHoraStop,
     required this.stopKm,
     required this.ganhos,
   });
@@ -16,7 +20,7 @@ class Day {
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'data': data,
+      'data': dataHoraStart,
       'start_km': startKm,
       'stop_km': stopKm,
       'ganhos': ganhos,
